d7827bac8c94b15f8149a8f61eedc3c381cbef38
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-05-21 15:21:27 +0200

    vidéo

---
+++ Assets/Script/movieTexture.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movieTexture : MonoBehaviour
+{
+    MovieTexture movie;
+
+    [FMODUnity.EventRef]
+    public string select_sound;
+    public FMOD.Studio.EventInstance soundevent;
+
+    void Start()
+    {
+        soundevent = FMODUnity.RuntimeManager.CreateInstance(select_sound);
+        soundevent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(this.transform));
+
+        movie = GetComponent<RawImage>().mainTexture as MovieTexture;
+    }
+
+    public void Activevideo()
+    {
+        GetComponent<RawImage>().enabled = true;
+        movie.Play();
+        FMOD.Studio.PLAYBACK_STATE fmodPbState;
+        soundevent.getPlaybackState(out fmodPbState);
+        if (fmodPbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            soundevent.start();
+        }
+
+        StartCoroutine("Continue");
+    }
+
+    IEnumerator Continue()
+    {
+        yield return new WaitForSeconds(8);
+        movie.Stop();
+        soundevent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        GameObject.Find("GameMaster").GetComponent<EnnemiManager>().SpawnAdversaire();
+        this.gameObject.SetActive(false);
+    }
+}
