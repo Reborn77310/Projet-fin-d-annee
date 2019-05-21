1eb45a8c2e45180fd75d51f607f6068828869945
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-05-21 17:24:19 +0200

    la seine

---
+++ Assets/NEST/position.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class position : MonoBehaviour
+{
+    void Start()
+    {        
+        var parent = transform.parent;
+        var index = parent.transform.parent.GetComponent<ModuleManager>().MySalleNumber;
+        var positionToGet = GameObject.Find("Console" + index).transform.position;
+        transform.position = positionToGet;
+    }
+}
