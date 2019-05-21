5319d7a70b9c525f6d8b2133865c54ba898866df
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-05-22 00:19:47 +0200

    premiere collab

--- Assets/Script/ComportementPersonnage.cs
+++ Assets/Script/ComportementPersonnage.cs
@@ -7,17 +7,126 @@ public class ComportementPersonnage : MonoBehaviour
 {
     public NavMeshAgent[] agents = new NavMeshAgent[2];
     public Animator[] agentsAnimator = new Animator[2];
+
+    public Transform[] ConsolesPositions = new Transform[4];
+    public Transform[] CommonPositions = new Transform[4];
+    public int NumberOfCategories = 2;
+
+    Vector3[] agentsDestination = new Vector3[2];
+
+    SalleManager salleManager;
+    EnnemiManager ennemiManager;
     void Awake()
     {
-        agents[0].SetDestination(new Vector3(-25.32268f,-2.87211f,-9.4f));
-        agents[1].SetDestination(new Vector3(-13.52f,-2.87211f,5.442418f));
+        salleManager = GameObject.Find("GameMaster").GetComponent<SalleManager>();
+        ennemiManager = GameObject.Find("GameMaster").GetComponent<EnnemiManager>();
+
+        FindFirstDestination();
     }
 
-    public void CheckState()
+    void FindFirstDestination()
     {
-        for(int i =0; i < agents.Length; i++)
-        {
+        agentsDestination[0] = SelectDirection();
+        agents[0].SetDestination(agentsDestination[0]);
 
+        agentsDestination[1] = SelectDirection();
+        while (agentsDestination[1] == agentsDestination[0])
+        {
+            agentsDestination[1] = SelectDirection();
         }
+        agents[1].SetDestination(agentsDestination[1]);
+    }
+
+    void Update()
+    {
+        CheckIfRunning();
+    }
+
+    void CheckIfRunning()
+    {
+        for (int i = 0; i < agents.Length; i++)
+        {
+            if (agents[i].remainingDistance > 0.2f && !agentsAnimator[i].GetBool("GoRun"))
+            {
+                agentsAnimator[i].SetBool("GoRun", true);
+            }
+            else if (agents[i].remainingDistance <= 0.2f && agentsAnimator[i].GetBool("GoRun"))
+            {
+
+                agentsAnimator[i].SetBool("GoRun", false);
+                StartCoroutine("StartNewDirection", i);
+            }
+        }
+    }
+
+    IEnumerator StartNewDirection(int i)
+    {
+        var randomTime = ReturnRandom(10);
+        yield return new WaitForSeconds(randomTime);
+
+        int a = 0;
+        if (i == 0)
+        {
+            a = 1;
+        }        
+
+        agentsDestination[i] = SelectDirection();
+        while (agentsDestination[i] == agentsDestination[a])
+        {
+            agentsDestination[i] = SelectDirection();
+        }
+        agents[i].SetDestination(agentsDestination[i]);
+        yield break;
+    }
+
+    bool CheckIfRoomIsPlayable()
+    {
+        bool canPlayHere = true;
+        for (int b = 0; b < ennemiManager.effetsEnnemi.Count; b++)
+        {
+            if (ennemiManager.effetsEnnemi[b].salle == 0)
+            {
+                var tags = ennemiManager.effetsEnnemi[b].tags;
+                for (int c = 0; c < tags.Length; c++)
+                {
+                    if (tags[c] == "CHIMIC" || tags[c] == "DOT")
+                    {
+                        canPlayHere = false;
+                    }
+                }
+            }
+        }
+        return canPlayHere;
+    }
+    Vector3 SelectDirection()
+    {
+        int random = ReturnRandom(NumberOfCategories);
+        var wantedPos = Vector3.zero;
+
+        if (random == 0)
+        {
+            wantedPos = PickRandomPositionWithArrays(ConsolesPositions);
+        }
+        else if (random == 1)
+        {
+            wantedPos = PickRandomPositionWithArrays(CommonPositions);
+        }
+        return wantedPos;
+    }
+
+    int ReturnRandom(int wantedRandomValue)
+    {
+        int toReturn = 0;
+        toReturn = Random.Range(0, wantedRandomValue);
+        return toReturn;
+    }
+
+    Vector3 PickRandomPositionWithArrays(Transform[] wantedTransform)
+    {
+        Vector3 toReturn = Vector3.zero;
+        int randomPos = ReturnRandom(wantedTransform.Length);
+
+        toReturn = wantedTransform[randomPos].position;
+        return toReturn;
     }
 }
