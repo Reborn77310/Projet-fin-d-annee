f4fd5178a3c02950c1f534999ae38b91333281f3
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-05-21 03:11:47 +0200

    p

---
+++ Assets/NEST/Missiles/missile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missile : MonoBehaviour
+{
+    public float Speed = 3;
+    public GameObject Explosion;
+
+
+    void Update()
+    {
+        transform.Translate(-transform.right * Time.deltaTime * Speed);
+    }
+
+    void OnCollisionEnter()
+    {
+        RaycastHit hit;
+        var direction = transform.position - transform.right;
+        if(Physics.Raycast(transform.position,direction,out hit))
+        {
+            print(hit.collider.name);
+            var pos = hit.point;
+            Instantiate(Explosion,pos,Quaternion.identity);
+        }
+        Destroy(this.gameObject);
+    }
+}
