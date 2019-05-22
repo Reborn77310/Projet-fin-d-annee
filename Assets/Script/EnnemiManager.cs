be31af170f9e6321c9ac05af351ae224126f8ad5
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-05-22 02:59:01 +0200

    oui

--- Assets/Script/EnnemiManager.cs
+++ Assets/Script/EnnemiManager.cs
@@ -932,24 +932,25 @@ public class EnnemiManager : MonoBehaviour
 
     IEnumerator Brulez(int i) //Jfais durer 10 sec vue que on peut pas l'annuler tfaçon
     {
-        salleManager.allSalles[effetsEnnemi[i].salle].MyGo.GetComponent<IncendieSound>().LaunchIncendie(); //SON
+        print(i);
+        salleManager.allSalles[i].MyGo.GetComponent<IncendieSound>().LaunchIncendie(); //SON
 
         GameObject go = Resources.Load("Prefabs/FeedbackActionsEnnemisSurNest/Incendie") as GameObject;
-        GameObject salleGo = salleManager.allSalles[effetsEnnemi[i].salle].MyGo;
+        GameObject salleGo = salleManager.allSalles[i].MyGo;
 
-        salleManager.DamageSurSalle(effetsEnnemi[i].salle, 35);
+        salleManager.DamageSurSalle(i, 35);
         yield return new WaitForSeconds(2);
 
         var newGo = Instantiate(go, salleGo.transform.position, go.transform.rotation, salleGo.transform);
         newGo.transform.localPosition = Vector3.zero;
 
-        salleManager.DamageSurSalle(effetsEnnemi[i].salle, 5);
+        salleManager.DamageSurSalle(i, 5);
         yield return new WaitForSeconds(2);
-        salleManager.DamageSurSalle(effetsEnnemi[i].salle, 5);
+        salleManager.DamageSurSalle(i, 5);
         yield return new WaitForSeconds(2);
-        salleManager.DamageSurSalle(effetsEnnemi[i].salle, 5);
+        salleManager.DamageSurSalle(i, 5);
         yield return new WaitForSeconds(2);
-        salleManager.DamageSurSalle(effetsEnnemi[i].salle, 5);
+        salleManager.DamageSurSalle(i, 5);
 
         foreach (Transform child in newGo.transform)
         {
