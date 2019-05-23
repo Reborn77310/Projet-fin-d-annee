3bf0225bdb4c80de9d77e2b761aecb6779a0e7a1
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-05-23 04:34:06 +0200

    grande collab

---
+++ Assets/Script/MusiqueScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusiqueScript : MonoBehaviour
+{
+    [FMODUnity.EventRef] public string PhaseLente;
+    public FMOD.Studio.EventInstance SoundEvent;
+    [FMODUnity.EventRef] public string Select_Sound2;
+    public FMOD.Studio.EventInstance SoundEvent2;
+    [FMODUnity.EventRef] public string Select_Sound3;
+    public FMOD.Studio.EventInstance SoundEvent3;
+
+    void Start()
+    {
+        SoundEvent = FMODUnity.RuntimeManager.CreateInstance(PhaseLente);
+        SoundEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(this.transform));
+    }
+
+    public void LancerPhaseLente()
+    {
+        FMOD.Studio.PLAYBACK_STATE fmodPbState;
+        SoundEvent.getPlaybackState(out fmodPbState);
+        SoundEvent.start();
+    }
+    public void StopPhaseLente()
+    {
+        SoundEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }  
+}
---
+++ Assets/Script/VideoTransmission.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VideoTransmission : MonoBehaviour
+{
+    public GameObject Transmission;
+    MovieTexture movie;
+
+    [FMODUnity.EventRef]
+    public string select_sound;
+    public FMOD.Studio.EventInstance soundevent;
+
+    void Awake()
+    {
+        soundevent = FMODUnity.RuntimeManager.CreateInstance(select_sound);
+        soundevent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(this.transform));
+    }
+
+    public void Activevideo()
+    {
+        GetComponent<RawImage>().enabled = true;
+        movie = GetComponent<RawImage>().mainTexture as MovieTexture;
+
+        movie.Play();
+        FMOD.Studio.PLAYBACK_STATE fmodPbState;
+        soundevent.getPlaybackState(out fmodPbState);
+
+        soundevent.start();
+
+        StartCoroutine("Continue");
+    }
+
+    IEnumerator Continue()
+    {
+        Transmission.SetActive(true);
+        yield return new WaitForSeconds(8);
+
+        movie.Stop();
+        soundevent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        
+        GameObject.Find("GameMaster").GetComponent<GameMaster>().etat = 2;
+        this.gameObject.SetActive(false);
+    }
+}
